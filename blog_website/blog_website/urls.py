a6ab240c6148ff3557799cae4d379778a76ec71a
Asif <asifhayatanjon@gmail.com>
2022-04-20 17:46:14 +0600

    update

--- blog_website/blog_website/urls.py
+++ blog_website/blog_website/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('',include('blog.urls')),
 ]
 
-urlpatterns+= static(settings.STATIC_URL, documant_root=settings.STATIC_ROOT)
-urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, documant_root=settings.STATIC_ROOT)
+urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
