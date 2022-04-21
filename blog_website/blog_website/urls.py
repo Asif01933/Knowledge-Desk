0c4170f7a653a123644ecccf4f8a8a33f3e3061f
Asif <asifhayatanjon@gmail.com>
2022-04-21 16:30:56 +0600

    update

--- blog_website/blog_website/urls.py
+++ blog_website/blog_website/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
+    path('',include('user_profile.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, documant_root=settings.STATIC_ROOT)
